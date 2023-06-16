4d891b76e46ad1ed2b6d6f237978c098759bbdf8
Rubén Zamora <ruben.zamora@leanmind.es>
2023-06-16 13:17:43 +0100

    safe commit after refactor

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -2,52 +2,98 @@ using FluentAssertions;
 
 namespace TestProject1;
 
+public enum DoorState
+{
+    Closed,
+    Opening,
+    Opened,
+    Closing
+}
+
 public class Door
 {
-    int doorPosition = 0;
-    bool doorIsOpening = false;
+    private const int FULLY_OPENED = 5;
+    private const int FULLY_CLOSED = 0;
+    private int _position = 0;
+    private bool _isOpening = false;
+    private DoorState _doorState = DoorState.Closed;
 
     public string ProcessEvents(string events)
     {
         return new string(
             events.ToCharArray()
-                .Select(c =>
+                .Select(@event =>
                 {
-                    if (c == 'P')
+                    SwitchBehaviourByBottomPressed(@event);
+                    if (_isOpening)
                     {
-                        doorIsOpening = !doorIsOpening;
-                    }
-                    
-                    if (doorIsOpening)
-                    {
-                        var MAX_APERTURE = 5;
-                        if (doorPosition == MAX_APERTURE)
+                        if (_position == FULLY_OPENED)
                         {
-                            return doorPosition.ToString()[0];
+                            return _position.ToString()[0];
                         }
-
-                        doorPosition++;
+                        _position++;
                     }
                     else
                     {
-                        var MIN_APERTURE = 0;
-                        if (doorPosition == MIN_APERTURE)
+                        if (_position == FULLY_CLOSED)
                         {
-                            return doorPosition.ToString()[0];
+                            return _position.ToString()[0];
                         }
-                        doorPosition--;
+                        _position--;
                     }
 
-                    return doorPosition.ToString()[0];
+                    return _position.ToString()[0];
                 })
                 .ToArray()
         );
     }
+    
+    /*
+    public string ProcessEvents(string events, bool new_way)
+    {
+        return new string(
+            events.ToCharArray()
+                .Select(@event =>
+                {
+                    SwitchBehaviourByBottomPressed(@event);
+                    var currentPosition = _position.ToString()[0];
+                    if (_isOpening)
+                    {
+                        if (_position == FULLY_OPENED)
+                        {
+                            return currentPosition;
+                        }
+                        _position++;
+                    }
+                    else
+                    {
+                        if (_position == FULLY_CLOSED)
+                        {
+                            return currentPosition;
+                        }
+                        _position--;
+                    }
+
+                    return currentPosition;
+                })
+                .ToArray()
+        );
+    }
+    */
+
+
+    private void SwitchBehaviourByBottomPressed(char @event)
+    {
+        if (@event == 'P')
+        {
+            _isOpening = !_isOpening;
+        }
+    }
 
     public void CleanUp()
     {
-        doorPosition = 0;
-        doorIsOpening = false;
+        _position = 0;
+        _isOpening = false;
     }
 }
 
@@ -118,18 +164,12 @@ public class DoorTestCases : IDisposable
         actual.Should().Be("12345554321000");
     }
 
-    [Fact]
-    public void NormalOperation_ShouldOpenCompletelyAndStayOpenThenClose()
-    {
-        var actual = _objectUnderTest.ProcessEvents("P......P......");
-        // //Assert.AreEqual("12345554321000", actual);
-    }
 
     [Fact]
     public void Pause_ShouldStartOpeningAndPauseOnSecondPress()
     {
         var actual = _objectUnderTest.ProcessEvents("P.P..");
-        //Assert.AreEqual("12222", actual);
+        actual.Should().Be("12222");
     }
 
     [Fact]
