49bcd74404c77e0bfd3c08d7e02280dc467e29a0
Ruben ZaGon-LeanMind <ruben.zamora@leanmind.es>
2023-06-29 15:06:39 +0100

    wip - parallel change

--- src/Killer.Garage.Door/States/Opening.cs
+++ src/Killer.Garage.Door/States/Opening.cs
@@ -50,6 +50,17 @@ public class Opening : State
             _garageDoor.ChangeState(new Pause(_garageDoor));
             return _garageDoor.position.ToString()[0];
         }
+
+        var isObstacleDetected = @event == 'O';
+        if (isObstacleDetected)
+        {
+            _garageDoor.ChangeState(new Closing(_garageDoor));
+            _garageDoor.lastDirection = new Closing(_garageDoor);
+            return (_garageDoor.position -= 1).ToString()[0]; // Me chirria que opening conozca el comportamiento de Closing
+        }
+
+        _garageDoor.ChangeState(new Opening(_garageDoor));
+        _garageDoor.lastDirection = new Opening(_garageDoor);
         return (_garageDoor.position += 1).ToString()[0];
     }
 }
